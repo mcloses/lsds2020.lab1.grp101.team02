19b54f13be79a913888c7964581be361c5552ccf
mcloses <46600175+mcloses@users.noreply.github.com>
2020-02-04 18:15:43 +0100

    Implemented SimplifiedTweet.java and tested it
    Main method is modified for testing

--- lab1/src/main/java/upf/edu/parser/SimplifiedTweet.java
+++ lab1/src/main/java/upf/edu/parser/SimplifiedTweet.java
@@ -3,13 +3,14 @@ package upf.edu.parser;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonElement;
 
 import java.io.Serializable;
 import java.util.Optional;
 
 public class SimplifiedTweet {
 
-  private static ?? parser = new ????;
+  private static JsonParser parser = new JsonParser();
 
   private final long tweetId;			  // the id of the tweet ('id')
   private final String text;  		      // the content of the tweet ('text')
@@ -21,7 +22,12 @@ public class SimplifiedTweet {
   public SimplifiedTweet(long tweetId, String text, long userId, String userName,
                          String language, long timestampMs) {
 
-    // PLACE YOUR CODE HERE!
+	this.tweetId = tweetId;
+	this.text = text;
+	this.userId = userId;
+	this.userName = userName;
+	this.language = language;
+	this.timestampMs = timestampMs;
 
   }
 
@@ -34,7 +40,37 @@ public class SimplifiedTweet {
    */
   public static Optional<SimplifiedTweet> fromJson(String jsonStr) {
 
-    // PLACE YOUR CODE HERE!
+	long tweetId = 0;		 
+	String text = "";      
+  	long userId = 0;		  
+ 	String userName = "";	  
+ 	String language = "";     
+  	long timestampMs = 0;
 
+	JsonElement je = SimplifiedTweet.parser.parse(jsonStr);
+	JsonObject jo = je.getAsJsonObject();
+
+	if (jo.has("lang"))
+	{
+
+		if (jo.has("id")){tweetId = jo.get("id").getAsLong();}
+		if (jo.has("text")){text = jo.get("text").getAsString();}
+		if (jo.has("user")){
+			JsonObject userObj = jo.get("user").getAsJsonObject();
+			if (userObj.has("id")){userId = userObj.get("id").getAsLong();}
+			if (userObj.has("name")){userName = userObj.get("name").getAsString();}
+		}
+		language = jo.get("lang").getAsString();
+		if(jo.has("timestamp_ms")){timestampMs = jo.get("timestamp_ms").getAsLong();}
+
+		SimplifiedTweet tweet = new SimplifiedTweet(tweetId, text, userId, userName, language, timestampMs);
+
+		return Optional.ofNullable(tweet);
+
+  	}
+	else
+	{
+		return Optional.ofNullable(null);
+	}
   }
 }
